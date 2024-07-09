8c2eff459bfb8d2d1580225c44bef6b3cc2ad285
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-07-09 17:16:33 +0900

    feat: update images.ts

--- types/images.ts
+++ types/images.ts
@@ -1,7 +1,17 @@
-export type DockerImageDetail = {
+export interface DockerImageDetail {
   id: string;
   name: string;
   tag: string;
+  description: string;
   createdAt: string;
   size: string;
-};
+  repository: string;
+  dockerVersion: string;
+  author: string;
+  labels: { [key: string]: string };
+  architecture: string;
+  os: string;
+  ports: string[];
+  environmentVariables: { [key: string]: string };
+  volumes: string[];
+}
