dd51ba98ea64d8924809f8304c917f8d3805fb6a
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-07-10 17:49:04 +0900

    style: change containerList style

--- src/app/components/containers/containerList.tsx
+++ src/app/components/containers/containerList.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useRouter } from "next/navigation";
 import React from "react";
 import { ContainerListProps } from "../../../../types/containers";
@@ -10,57 +12,55 @@ const ContainerList: React.FC<ContainerListProps> = ({
 }) => {
   const router = useRouter();
 
-  const handleRowClick = (id: string) => {
-    // router.push(`/containers/detail`);
+  const handleCardClick = (id: string) => {
     router.push(`/containers/${id}`);
   };
 
   return (
     <div className="container p-4 mx-auto text-black">
       <h1 className="mb-4 text-2xl font-bold">Docker Containers</h1>
-      <table className="min-w-full bg-white">
-        <thead>
-          <tr>
-            <th className="py-2">ID</th>
-            <th className="py-2">Name</th>
-            <th className="py-2">Status</th>
-            <th className="py-2">Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {containers.map((container) => (
-            <tr
-              key={container.id}
-              className="border-t"
-              onClick={() => handleRowClick(container.id)}
-            >
-              <td className="py-2 text-center">{container.id}</td>
-              <td className="py-2 text-center">{container.name}</td>
-              <td className="py-2 text-center">{container.status}</td>
-              <td className="py-2 space-x-2 text-center">
-                <button
-                  onClick={() => onStart(container.id)}
-                  className="px-2 py-1 text-white bg-green-500 rounded"
-                >
-                  Start
-                </button>
-                <button
-                  onClick={() => onStop(container.id)}
-                  className="px-2 py-1 text-white bg-yellow-500 rounded"
-                >
-                  Stop
-                </button>
-                <button
-                  onClick={() => onDelete(container.id)}
-                  className="px-2 py-1 text-white bg-red-500 rounded"
-                >
-                  Delete
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        {containers.map((container) => (
+          <div
+            key={container.id}
+            className="p-4 bg-white border rounded shadow cursor-pointer"
+            onClick={() => handleCardClick(container.id)}
+          >
+            <div className="mb-2 text-xl font-bold">{container.name}</div>
+            <div className="mb-2 text-gray-700">ID: {container.id}</div>
+            <div className="mb-4 text-gray-700">Status: {container.status}</div>
+            <div className="space-x-2">
+              <button
+                onClick={(e) => {
+                  e.stopPropagation();
+                  onStart(container.id);
+                }}
+                className="px-2 py-1 text-white bg-green-500 rounded"
+              >
+                Start
+              </button>
+              <button
+                onClick={(e) => {
+                  e.stopPropagation();
+                  onStop(container.id);
+                }}
+                className="px-2 py-1 text-white bg-yellow-500 rounded"
+              >
+                Stop
+              </button>
+              <button
+                onClick={(e) => {
+                  e.stopPropagation();
+                  onDelete(container.id);
+                }}
+                className="px-2 py-1 text-white bg-red-500 rounded"
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
