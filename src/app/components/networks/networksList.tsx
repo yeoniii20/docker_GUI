cef914b26829c2ff8569f1bb058643c657e3de0b
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-07-10 17:50:24 +0900

    style: change NetworksList style

--- src/app/components/networks/networksList.tsx
+++ src/app/components/networks/networksList.tsx
@@ -7,44 +7,37 @@ import { NetworkListProps } from "../../../../types/networks";
 const NetworkList: React.FC<NetworkListProps> = ({ networks, onDelete }) => {
   const router = useRouter();
 
-  const handleRowClick = (id: string) => {
+  const handleCardClick = (id: string) => {
     router.push(`/networks/${id}`);
   };
 
   return (
     <div className="container p-4 mx-auto text-black">
       <h1 className="mb-4 text-2xl font-bold">Docker Networks</h1>
-      <table className="min-w-full bg-white">
-        <thead>
-          <tr>
-            <th className="py-2">ID</th>
-            <th className="py-2">Name</th>
-            <th className="py-2">Driver</th>
-            <th className="py-2">Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {networks.map((network) => (
-            <tr
-              key={network.id}
-              className="border-t"
-              onClick={() => handleRowClick(network.id)}
-            >
-              <td className="py-2 text-center">{network.id}</td>
-              <td className="py-2 text-center">{network.name}</td>
-              <td className="py-2 text-center">{network.driver}</td>
-              <td className="py-2 space-x-2 text-center">
-                <button
-                  onClick={() => onDelete(network.id)}
-                  className="px-2 py-1 text-white bg-red-500 rounded"
-                >
-                  Delete
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        {networks.map((network) => (
+          <div
+            key={network.id}
+            className="p-4 bg-white border rounded shadow cursor-pointer"
+            onClick={() => handleCardClick(network.id)}
+          >
+            <div className="mb-2 text-xl font-bold">{network.name}</div>
+            <div className="mb-2 text-gray-700">ID: {network.id}</div>
+            <div className="mb-4 text-gray-700">Driver: {network.driver}</div>
+            <div className="space-x-2">
+              <button
+                onClick={(e) => {
+                  e.stopPropagation();
+                  onDelete(network.id);
+                }}
+                className="px-2 py-1 text-white bg-red-500 rounded"
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
