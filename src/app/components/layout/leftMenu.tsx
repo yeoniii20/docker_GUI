e95d55ab90484afccd2df0a4177335f7dc74cdc4
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-07-12 13:28:22 +0900

    feat: add icon at left menu

--- src/app/components/layout/leftMenu.tsx
+++ src/app/components/layout/leftMenu.tsx
@@ -6,9 +6,10 @@ import { MENU_ITEMS } from "../../../../data/common";
 
 interface LeftMenuProps {
   isOpen: boolean;
+  isMdOrSmaller: boolean;
 }
 
-const LeftMenu: React.FC<LeftMenuProps> = ({ isOpen }) => {
+const LeftMenu: React.FC<LeftMenuProps> = ({ isOpen, isMdOrSmaller }) => {
   const router = useRouter();
   const pathname = usePathname();
 
@@ -24,29 +25,39 @@ const LeftMenu: React.FC<LeftMenuProps> = ({ isOpen }) => {
   };
 
   return (
-    <>
-      <aside
-        className={`fixed inset-y-0 left-0 w-64 bg-gray-200 p-4 transform ${
-          isOpen ? "translate-x-0" : "-translate-x-full"
-        } transition-transform duration-300 ease-in-out lg:translate-x-0 lg:static z-9 `}
-      >
-        <nav className="space-y-4 mt-16 lg:mt-0">
-          {MENU_ITEMS.map((item) => (
+    <aside
+      className={`fixed inset-y-0 left-0 ${
+        isMdOrSmaller ? "w-16" : "w-64"
+      } bg-gray-200 p-4 transform ${
+        isOpen ? "translate-x-0" : "-translate-x-full"
+      } transition-transform duration-300 ease-in-out z-9`}
+    >
+      <nav className="space-y-4 mt-16">
+        {MENU_ITEMS.map((item) => {
+          const Icon = item.icon;
+          const active = isActive(item.path);
+          return (
             <div
               key={item.path}
-              className={`block py-2 px-4 rounded cursor-pointer ${
-                isActive(item.path)
+              className={`${
+                isMdOrSmaller ? "" : "px-4"
+              } gap-2 py-2 rounded cursor-pointer flex items-center  ${
+                active
                   ? "bg-blue-400 text-white"
                   : "text-gray-700 hover:bg-gray-300"
               }`}
               onClick={() => navigateTo(item.path)}
             >
-              {item.name}
+              <Icon
+                className={`${active ? "text-white" : "text-gray-700"} pl-2`}
+                size={24}
+              />
+              {!isMdOrSmaller && <span>{item.name}</span>}
             </div>
-          ))}
-        </nav>
-      </aside>
-    </>
+          );
+        })}
+      </nav>
+    </aside>
   );
 };
 
