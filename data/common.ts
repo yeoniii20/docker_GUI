3945803661179b3eb850ff9a84c3a5a7a3a78fb2
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-07-12 13:26:59 +0900

    feat: add icon at common.ts

--- data/common.ts
+++ data/common.ts
@@ -1,7 +1,15 @@
+import {
+  FaHome,
+  FaBox,
+  FaImages,
+  FaNetworkWired,
+  FaDatabase,
+} from "react-icons/fa";
+
 export const MENU_ITEMS = [
-  { name: "Home", path: "/" },
-  { name: "Containers", path: "/containers" },
-  { name: "Images", path: "/images" },
-  { name: "Networks", path: "/networks" },
-  { name: "Volumes", path: "/volumes" },
+  { name: "Home", path: "/", icon: FaHome },
+  { name: "Containers", path: "/containers", icon: FaBox },
+  { name: "Images", path: "/images", icon: FaImages },
+  { name: "Networks", path: "/networks", icon: FaNetworkWired },
+  { name: "Volumes", path: "/volumes", icon: FaDatabase },
 ];
