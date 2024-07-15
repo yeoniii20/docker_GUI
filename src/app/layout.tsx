79c360993edc556fe6792e0b591e6eef96dce361
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-07-15 16:53:04 +0900

    feat: add container provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import LeftMenu from "./components/layout/leftMenu";
 import Header from "./components/layout/header";
 import React, { useState } from "react";
 import { useMediaQuery } from "react-responsive";
+import { ContainerProvider } from "../../context/containerContext";
 
 export default function RootLayout({
   children,
@@ -21,19 +22,21 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <div className="min-h-screen flex flex-col">
-          <Header toggleMenu={toggleMenu} />
-          <div className="flex flex-1 mt-14">
-            <LeftMenu isOpen={isMenuOpen} isMdOrSmaller={isMdOrSmaller} />
-            <main
-              className={`flex-1 p-2 transition-all duration-300 ease-in-out ${
-                isMenuOpen ? (isMdOrSmaller ? "ml-16" : "ml-64") : "ml-0"
-              }`}
-            >
-              {children}
-            </main>
+        <ContainerProvider>
+          <div className="min-h-screen flex flex-col">
+            <Header toggleMenu={toggleMenu} />
+            <div className="flex flex-1 mt-14">
+              <LeftMenu isOpen={isMenuOpen} isMdOrSmaller={isMdOrSmaller} />
+              <main
+                className={`flex-1 p-2 transition-all duration-300 ease-in-out ${
+                  isMenuOpen ? (isMdOrSmaller ? "ml-16" : "ml-64") : "ml-0"
+                }`}
+              >
+                {children}
+              </main>
+            </div>
           </div>
-        </div>
+        </ContainerProvider>
       </body>
     </html>
   );
